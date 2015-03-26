567327e56ea7b6b423d763d0114b294d3f455b3b
Ray Villalobos <ray@planetoftheweb.com>
2015-03-26 16:28:38 -0400

    Bootstrap Layout Done, Original Data file

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,87 +1,50 @@
-$(function() {
+"use strict";
 
-  "use strict";
+document.rangeform.onsubmit=function() {
 
-  var topoffset = 50; //variable for menu height
-  var slideqty = $('#featured .item').length;
-  var wheight = $(window).height(); //get the height of the window
-  var randSlide = Math.floor(Math.random()*slideqty);
+  var from = document.rangeform.from.value;
+  var to = document.rangeform.to.value;
 
-  $('#featured .item').eq(randSlide).addClass('active');
-
-
-  $('.fullheight').css('height', wheight); //set to window tallness  
+  	console.log(from + '--' +to)
+  	return false;
+};
 
 
-  //replace IMG inside carousels with a background image
-  $('#featured .item img').each(function() {
-    var imgSrc = $(this).attr('src');
-    $(this).parent().css({'background-image': 'url('+imgSrc+')'});
-    $(this).remove();
-  });
-
-  //adjust height of .fullheight elements on window resize
-  $(window).resize(function() {
-    wheight = $(window).height(); //get the height of the window
-    $('.fullheight').css('height', wheight); //set to window tallness  
-  });
-
-
-
-  //Activate Scrollspy
-  $('body').scrollspy({
-    target: 'header .navbar',
-    offset: topoffset
-  });
-
-  // add inbody class
-  var hash = $(this).find('li.active a').attr('href');
-  if(hash !== '#featured') {
-    $('header nav').addClass('inbody');
-  } else {
-    $('header nav').removeClass('inbody');
-  }
-
-
-  // Add an inbody class to nav when scrollspy event fires
-  $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
-    var hash = $(this).find('li.active a').attr('href');
-    if(hash !== '#featured') {
-      $('header nav').addClass('inbody');
-    } else {
-      $('header nav').removeClass('inbody');
-    }
-  });
-
-
-  //Use smooth scrolling when clicking on navigation
-  $('.navbar a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
-      location.hostname === this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top-topoffset+2
-        }, 500);
-        return false;
-      } //target.length
-    } //click function
-  }); //smooth scrolling
-
-  //Automatically generate carousel indicators
-  for (var i=0; i < slideqty; i++) {
-    var insertText = '<li data-target="#featured" data-slide-to="' + i + '"';
-    if (i === randSlide) {
-      insertText += ' class="active" ';
-    }
-    insertText += '></li>';
-    $('#featured ol').append(insertText);
-  }
-
-  $('.carousel').carousel({
-    pause: false
-  });
-
+var chart = c3.generate({
+    data: {
+        x : 'date_time',
+			  xFormat: '%Y-%m-%d %H:%M:%S',
+				url: 'data/2011.csv',
+				type: 'bar',
+        groups: [
+            ['Air_Temp','Barometric_Press','Wind_Speed']
+        ],
+        hide: ['Dew_Point','Relative_Humidity','Wind_Dir','Wind_Gust'],
+			  onclick: function (d) { 
+			  	console.log(d);
+			  }
+    },
+    color: {
+        pattern: ['#C94C24', '#2D8BCF', '#A2CEA5', 'transparent']
+    },
+        subchart: {
+        show: true
+    },
+    axis: {
+        x: {
+            type: 'category',
+            tick: {
+                count: 12,
+                format: '%Y-%m-%d %H:%M:%S',
+					      rotate: 0,
+								culling: {
+                    max: 12 // the number of tick texts will be adjusted to less than this value
+                }
+            }
+        }
+    },
+    legend: {
+			  hide: ['Dew_Point','Relative_Humidity','Wind_Dir','Wind_Gust'],
+			  position: 'bottom'
+		}
 });
