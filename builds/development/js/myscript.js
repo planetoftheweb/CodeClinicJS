3e9f8be804f5d79d3210c4f6763e1fd0dbcb6e11
Ray Villalobos <ray@lynda.com>
2015-04-08 22:28:55 -0400

    02_04 added drag and drop:

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,1 +1,40 @@
+$(function() {
 
+  'use strict';
+  var file, droppedImage;
+  var target = $('.dropzone');
+
+  function dropZone(target) {
+    target
+      .on('dragover', function() {
+        target.addClass('dragover');
+        return false;
+      })
+      .on('dragend', function() {
+        target.removeClass('dragover');
+        return false;
+      })
+      .on('dragleave', function() {
+        target.removeClass('dragover');
+        return false;
+      })
+      .on('drop', function(e) {
+        var fileReader;
+        file = e.originalEvent.dataTransfer.files[0];
+        e.stopPropagation();
+        e.preventDefault();
+        target.removeClass('dragover');
+
+        droppedImage = new Image();
+        fileReader = new FileReader();
+        fileReader.onload = function(e) {
+          droppedImage.src = e.target.result;
+          target.html(droppedImage);
+        };
+        fileReader.readAsDataURL(file);
+      }); // on drop
+  } //drop zone
+
+  dropZone(target);
+
+}); // page loaded
