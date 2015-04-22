c697f3f9ab4a08ce646ee5d2456d8808439a0782
Ray Villalobos <ray@planetoftheweb.com>
2015-04-22 10:31:49 -0400

    captions working

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,12 +1,8 @@
 $(function() {
   'use strict';
 
-  var documentNode = document.querySelector('#status');
-
-  // Compare Strings
-  function strComp(a, b) {
-    return (a > b) ? 1 : (a === b) ? 0 : -1;
-  }
+  var documentNode = document.querySelector('#dropzone');
+  var imageList = [];
 
   //When someone puts an image in the app
   document.querySelector('.app').addEventListener('dragenter', function(e) {
@@ -23,64 +19,31 @@ $(function() {
     e.preventDefault();
     var files = e.dataTransfer.files; //get the files from the drop event
 
-    //Loop should go here
+    //Loop through all of the dropped images
+    for (var i = 0; i < files.length; i++) {
+      //Read files as an image to display in the DOM
+      var fileAsImage = new FileReader();
+      var fileAsData = new FileReader();
 
-    //Read files as an image to display in the DOM
-    var fileAsImage = new FileReader();
-    fileAsImage.readAsDataURL(files[0]);
+      fileAsImage.readAsDataURL(files[i]);
+      fileAsData.file = files[i];
+      fileAsData.readAsBinaryString(files[i]);
 
-    fileAsImage.onloadend = function() {
-      var newImage = document.createElement('img');
-      newImage.src = this.result;
-      documentNode.appendChild(newImage);
+      //when the images are done loading as files
+      fileAsImage.onloadend = function() {
+        var newImage = document.createElement('img');
+        newImage.src = this.result;
+        documentNode.appendChild(newImage);
+      };// read the files as an image
 
       //Read the dropped images as data and extract the EXIF info
-      var fileAsData = new FileReader();
-      fileAsData.file = files[0];
-      fileAsData.readAsBinaryString(files[0]);
-
       fileAsData.onloadend = function() {
         var jpeg = new JpegMeta.JpegFile(this.result, this.file.name);
-        var groups = [];
-        var props;
-        var group;
-        var prop;
-        documentNode.innerHTML += 'JPEG File ' + jpeg + '<br />';
-
-        for (group in jpeg.metaGroups) {
-          if (jpeg.metaGroups.hasOwnProperty(group)) {
-            groups.push(jpeg.metaGroups[group]);
-          }
+        if (jpeg.iptc.caption.value!==undefined) {
+          console.log(jpeg.iptc.caption.value);
         }
-
-        groups.sort(function(a, b) {
-          if (a.description === 'General') {
-            return -1;
-          } else if (b.description === 'General') {
-            return 1;
-          } else {
-            return strComp(a.description, b.description);
-          }
-        });
-
-        for (var i = 0; i < groups.length; i++) {
-          group = groups[i];
-          props = [];
-          documentNode.innerHTML += '<h4>' + group.description + '</h4>';
-          for (prop in group.metaProps) {
-            if (group.metaProps.hasOwnProperty(prop)) {
-              props.push(group.metaProps[prop]);
-            }
-          }
-          props.sort(function(a, b) {
-            return strComp(a.description, b.description);
-          });
-          for (var j = 0; j < props.length; j++) {
-            prop = props[j];
-            documentNode.innerHTML += '<em>' + prop.description + ':</em> ' + prop.value + '<br />';
-          }
-        } // Loop through groups
-      }; //Read the files as data and extract EXIF info
-    };// read the files as an image
+      }; //Read the files as data and extract EXIF
+    } // loop through files
   }); // Files Dropped
+
 }); //page loaded
