f53623a94106bb8dcc44f48c0d40513a9072a7b5
Ray Villalobos <ray@lynda.com>
2015-04-22 22:06:15 -0400

    05_03end

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -1,4 +1,23 @@
 $(function() {
   'use strict';
 
+  var dropNode = document.querySelector('.app');
+  var files;
+
+
+  dropNode.addEventListener('dragenter', function(e) {
+    e.preventDefault();
+  });
+
+  dropNode.addEventListener('dragover', function(e) {
+    e.preventDefault();
+  });
+
+  dropNode.addEventListener('drop', function(e) {
+    e.preventDefault();
+    files = e.dataTransfer.files; //get the files from the drop event.
+    console.log(files);
+  });
+
+
 }); //page loaded
