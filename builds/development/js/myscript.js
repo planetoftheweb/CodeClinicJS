789ae36c2da8ca1e6f68954b40fc49fab9da18a6
Ray Villalobos <ray@lynda.com>
2015-04-22 23:23:52 -0400

    05_04 Displaying Images

--- builds/development/js/myscript.js
+++ builds/development/js/myscript.js
@@ -2,6 +2,7 @@ $(function() {
   'use strict';
 
   var dropNode = document.querySelector('.app');
+  var documentNode = document.querySelector('.dropzone');
   var files;
 
 
@@ -16,8 +17,19 @@ $(function() {
   dropNode.addEventListener('drop', function(e) {
     e.preventDefault();
     files = e.dataTransfer.files; //get the files from the drop event.
-    console.log(files);
-  });
+    
+    for (var i = 0; i < files.length; i++) {
+      var fileAsImage = new FileReader();
+      fileAsImage.readAsDataURL(files[i]);
+
+      fileAsImage.onloadend = function() {
+        var newImage = document.createElement('img');
+        newImage.src = this.result;
+        documentNode.appendChild(newImage);
+        console.log(i);
+      }; // file is finished loading
+    } // loop through the files
+  }); // Files dropped on browser
 
 
 }); //page loaded
